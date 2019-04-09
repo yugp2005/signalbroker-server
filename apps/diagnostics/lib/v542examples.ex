f187044a2856d03e96c7eb1476cdfc21560db418
Aleksandar Filipov <aleksandar.filipov@volvocars.com>
2019-04-09 21:05:42 +0200

    clean out irrelevant code

--- apps/diagnostics/lib/v542examples.ex
+++ apps/diagnostics/lib/v542examples.ex
@@ -19,16 +19,6 @@
 
 defmodule V542Examples do
 
-  # id 2047/0x7FF CemToAllFuncBodyDiagReqFrame
-  # id 1809/0x711 CemToCcmBodyDiagReqFrame def v542_read_in_car_temp() do
-    #   setup_diagnostics("CemToAllFuncBodyDiagReqFrame", "CcmToCemBodyDiagResFrame", flow_mode: :auto)
-    #   # 3 bytes
-    #   # 0x22 read data by identinifier (Service id)
-    #   # 0x1f90 did for vin number (Data identifier)
-    #   sendraw(<<0x03, @read_data_by_identifier 0xDD, 0x04, 0x00, 0x00, 0x00, 0x00>>)
-    # end
-  # id 1553/0x611 CcmToCemBodyDiagResFrame
-
   # operates on BodyCANhs, barely tested
 
   @read_data_by_identifier 0x22
@@ -76,27 +66,6 @@ defmodule V542Examples do
     # 3 bytes
     # 0x22 read data by identinifier (Service id)
     Diagnostics.sendraw(<<0x03, @read_data_by_identifier, 0x99A3::size(16), 0x00, 0x00, 0x00, 0x00>>)
-  end
-
-  def v542_break() do
-    Diagnostics.setup_diagnostics("CemToAllFuncBodyDiagReqFrame", "CcmToCemBodyDiagResFrame", flow_mode: :auto)
-    # 3 bytes
-    # 0x22 read data by identinifier (Service id)
-    Diagnostics.sendraw(<<0x03, @read_data_by_identifier, 0x404E::size(16), 0x00, 0x00, 0x00, 0x00>>)
-  end
-
-  def v542_key() do
-    Diagnostics.setup_diagnostics("CemToAllFuncBodyDiagReqFrame", "CcmToCemBodyDiagResFrame", flow_mode: :auto)
-    # 3 bytes
-    # 0x22 read data by identinifier (Service id)
-    Diagnostics.sendraw(<<0x03, @read_data_by_identifier, 0x411F::size(16), 0x00, 0x00, 0x00, 0x00>>)
-  end
-
-  def v542_accpedal() do
-    Diagnostics.setup_diagnostics("CemToAllFuncBodyDiagReqFrame", "CcmToCemBodyDiagResFrame", flow_mode: :auto)
-    # 3 bytes
-    # 0x22 read data by identinifier (Service id)
-    Diagnostics.sendraw(<<0x03, @read_data_by_identifier, 0xF449::size(16), 0x00, 0x00, 0x00, 0x00>>)
   end
 
 end
--- apps/diagnostics/lib/v60examples.ex
+++ apps/diagnostics/lib/v60examples.ex
@@ -19,16 +19,6 @@
 
 defmodule V60Examples do
 
-  # id 2015/0x7DF TesterFunctionalReqHS
-  # id 1830/0x726 TesterPhysicalReqCEMHSdef v60_read_in_car_temp() do
-    #   setup_diagnostics("TesterFunctionalReqHS", "TesterPhysicalResCEMHS", flow_mode: :auto)
-    #   # 3 bytes
-    #   # 0x22 read data by identinifier (Service id)
-    #   # 0x1f90 did for vin number (Data identifier)
-    #   sendraw(<<0x03, @read_data_by_identifier 0xDD, 0x04, 0x00, 0x00, 0x00, 0x00>>)
-    # end
-  # id 1838/0x72E TesterPhysicalResCEMHS
-
   @read_data_by_identifier 0x22
 
   def v60_read_vin() do
@@ -76,33 +66,4 @@ defmodule V60Examples do
     Diagnostics.sendraw(<<0x03, @read_data_by_identifier, 0x99A3::size(16), 0x00, 0x00, 0x00, 0x00>>)
   end
 
-  def v60_break() do
-    Diagnostics.setup_diagnostics("TesterFunctionalReqHS", "TesterPhysicalResCEMHS", flow_mode: :auto)
-    # 3 bytes
-    # 0x22 read data by identinifier (Service id)
-    Diagnostics.sendraw(<<0x03, @read_data_by_identifier, 0x404E::size(16), 0x00, 0x00, 0x00, 0x00>>)
-  end
-
-  def v60_key() do
-    Diagnostics.setup_diagnostics("TesterFunctionalReqHS", "TesterPhysicalResCEMHS", flow_mode: :auto)
-    # 3 bytes
-    # 0x22 read data by identinifier (Service id)
-    Diagnostics.sendraw(<<0x03, @read_data_by_identifier, 0x411F::size(16), 0x00, 0x00, 0x00, 0x00>>)
-  end
-
-  def v60_accpedal() do
-    Diagnostics.setup_diagnostics("TesterFunctionalReqHS", "TesterPhysicalResCEMHS", flow_mode: :auto)
-    # 3 bytes
-    # 0x22 read data by identinifier (Service id)
-    Diagnostics.sendraw(<<0x03, @read_data_by_identifier, 0xF449::size(16), 0x00, 0x00, 0x00, 0x00>>)
-  end
-
-  # kupenät
-  # def v60_CCM_cabin_temp() do
-  #   Diagnostics.setup_diagnostics("TesterPhysicalReqCCM", "TesterPhysicalResCCM", flow_mode: :auto)
-  #   # 3 bytes
-  #   # 0x22 read data by identinifier (Service id)
-  #   # 0x1f90 did for vin number (Data identifier)
-  #   Diagnostics.sendraw(<<0x03, @read_data_by_identifier 0x9A1B::size(16), 0x00, 0x00, 0x00, 0x00>>)
-  # end
 end
