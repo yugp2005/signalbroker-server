ba27f04959e1d9b7e5be0198f7019cfe795fed6d
Aleksandar Filipov <aleksandar.filipov@volvocars.com>
2019-08-29 15:46:02 +0200

    timing test

--- examples/grpc/python/simple_example.py
+++ examples/grpc/python/simple_example.py
@@ -37,6 +37,10 @@ import common_pb2
 import diagnostics_api_pb2_grpc
 import diagnostics_api_pb2
 
+from threading import Thread
+import threading, time
+
+
 
 def set_fan_speed(stub, value, freq):
     source_g = common_pb2.ClientId(id="app_identifier")
@@ -101,38 +105,50 @@ def subscribe_to_arbitration(stub):
             print(err)
 
 # make sure you have VirtualCanInterface namespace in interfaces.json
-def publish_signals(stub):
-    source = common_pb2.ClientId(id="app_identifier")
-    namespace = common_pb2.NameSpace(name = "VirtualCanInterface")
 
-    signal = common_pb2.SignalId(name="BenchC_c_5", namespace=namespace)
+sampletime = 0
+
+def publish_signals(stub):
+    global sampletime
+    millis = int(round(time.time() * 1000))
+    print(millis - sampletime)
+    sampletime = millis
+
+    source = common_pb2.ClientId(id="app_identifier")
+    namespace = common_pb2.NameSpace(name = "Chassi")
+
+    signal = common_pb2.SignalId(name="ECMChasFr03", namespace=namespace)
     signal_with_payload = network_api_pb2.Signal(id = signal)
     signal_with_payload.integer = 4
 
-    signal2 = common_pb2.SignalId(name="BenchC_c_2", namespace=namespace)
-    signal_with_payload_2 = network_api_pb2.Signal(id = signal2)
-    signal_with_payload_2.double = 3.4
+    # signal2 = common_pb2.SignalId(name="BenchC_c_2", namespace=namespace)
+    # signal_with_payload_2 = network_api_pb2.Signal(id = signal2)
+    # signal_with_payload_2.double = 3.4
+    #
+    # signal3 = common_pb2.SignalId(name="BenchC_d_2", namespace=namespace)
+    # signal_with_payload_3 = network_api_pb2.Signal(id = signal3)
+    # signal_with_payload_3.arbitration = True
 
-    signal3 = common_pb2.SignalId(name="BenchC_d_2", namespace=namespace)
-    signal_with_payload_3 = network_api_pb2.Signal(id = signal3)
-    signal_with_payload_3.arbitration = True
-
-    publisher_info = network_api_pb2.PublisherConfig(clientId = source, signals=network_api_pb2.Signals(signal=[signal_with_payload, signal_with_payload_2]), frequency = 0)
+    publisher_info = network_api_pb2.PublisherConfig(clientId = source, signals=network_api_pb2.Signals(signal=[signal_with_payload]), frequency = 0)
     try:
         stub.PublishSignals(publisher_info)
-        time.sleep(1)
+        #time.sleep(1)
     except grpc._channel._Rendezvous as err:
         print(err)
 
+def do_something(network_stub):
+    ticker = threading.Event()
+    while not ticker.wait(0.010):
+       publish_signals(network_stub)
 
 def run():
-    channel = grpc.insecure_channel('localhost:50051')
+    channel = grpc.insecure_channel('wicebox-debian:50051')
     functional_stub = functional_api_pb2_grpc.FunctionalServiceStub(channel)
     network_stub = network_api_pb2_grpc.NetworkServiceStub(channel)
     diag_stub = diagnostics_api_pb2_grpc.DiagnosticsServiceStub(channel)
 
-    print("-------------- Subsribe to fan speed BLOCKING --------------")
-    subscribe_to_fan_signal(network_stub)
+    # print("-------------- Subsribe to fan speed BLOCKING --------------")
+    # subscribe_to_fan_signal(network_stub)
 
     # print("-------------- Read Diagnostics --------------")
     # read_diagnostics_vin(diag_stub)
@@ -143,8 +159,15 @@ def run():
     # print("-------------- Subsribe to LIN arbitratin BLOCKING --------------")
     # subscribe_to_arbitration(network_stub)
     #
-    # print("-------------- Publish signals ONLY once--------------")
-    # publish_signals(network_stub)
+    print("-------------- Publish signals with 10 ms--------------")
+
+    repeat = Thread(
+            target = do_something,
+            args = (network_stub,))
+    repeat.start()
+
+
+
     #
     # print("-------------- SetFanSpeed --------------")
     # set_fan_speed(functional_stub, 8, 0)
